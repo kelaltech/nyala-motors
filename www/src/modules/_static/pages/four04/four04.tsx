7c77639af05bde3b1bfed38e0499069c8b110513
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2020-04-30 20:18:38 +0300

    Implement 404 page

--- www/src/modules/_static/pages/four04/four04.tsx
+++ www/src/modules/_static/pages/four04/four04.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { Content, Block } from 'gerami'
 
 import SEO from '../../../../shared/components/seo/seo'
 import Layout from '../../../../shared/components/layout/layout'
+import Towing from '../../../../assets/images/four04/towing.svg'
 
 type Four4 = {}
 
@@ -11,7 +13,28 @@ const Four04: React.FC<Four4> = () => {
       <SEO title="Home" />
 
       <Layout headerProps={{ mode: 'default' }}>
-        <>TODO: Four04</>
+        <Content
+          transparent
+          size="L"
+          className="margin-vertical-very-big padding-vertical-very-big"
+        >
+          <Block first last>
+            <Towing width="100%" height="auto" />
+          </Block>
+
+          <Block first last>
+            <h2 className="center">
+              <span className="middle">404</span>
+              <span
+                className="middle padding-big light"
+                style={{ opacity: 0.14, fontSize: `200%` }}
+              >
+                |
+              </span>
+              <span className="middle">Page Not Found</span>
+            </h2>
+          </Block>
+        </Content>
       </Layout>
     </>
   )
