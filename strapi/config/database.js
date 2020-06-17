b7b039a371740db2b5d5e7376f899070f11bc554
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2020-06-17 15:56:57 +0300

    Fix strapi database parsing undefined params issue

--- strapi/config/database.js
+++ strapi/config/database.js
@@ -6,10 +6,13 @@ module.exports = ({ env }) => {
   const parsed = new ConnectionString(
     env(
       'DATABASE_URL',
-      `postgres://${env('DATABASE_USERNAME', 'postgres')}:${env('DATABASE_PASSWORD', 'admin')}@${env(
-        'DATABASE_HOST',
-        '127.0.0.1'
-      )}:${env.int('DATABASE_PORT', 5432)}/${env('DATABASE_NAME', 'nyala-motors')}?ssl=${env.bool(
+      `postgres://${env('DATABASE_USERNAME', 'postgres')}:${env(
+        'DATABASE_PASSWORD',
+        'admin'
+      )}@${env('DATABASE_HOST', '127.0.0.1')}:${env.int(
+        'DATABASE_PORT',
+        5432
+      )}/${env('DATABASE_NAME', 'nyala-motors')}?ssl=${env.bool(
         'DATABASE_SSL',
         false
       )}`
@@ -28,7 +31,7 @@ module.exports = ({ env }) => {
           host: parsed.hostname,
           port: parsed.port,
           database: parsed.path.join('/'),
-          ssl: parsed.params.ssl === 'true',
+          ssl: parsed.params && parsed.params.ssl === 'true',
         },
         options: {},
       },
