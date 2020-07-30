0bf0b3ac9c8d9e68c5c767c349595cbb4e754755
BIRUK TESFAYE <biruktesfayeve@gmail.com>
2020-07-30 20:24:26 +0300

    modify product detail style

--- www/src/modules/product/pages/product-detail/product-detail.tsx
+++ www/src/modules/product/pages/product-detail/product-detail.tsx
@@ -1,8 +1,5 @@
 import React from 'react'
 import './product-detail.scss'
-// import 'aframe'
-// import 'aframe-particle-system-component'
-// import { Scene, Entity } from 'aframe-react';
 import { usePage } from '../../../../app/contexts/page-context/page-context'
 import qs from 'qs'
 import { useProductDetailQuery } from '../../../../app/graphql'
@@ -60,9 +57,7 @@ const ProductDetail: React.FC<ProductDetailProps> = () => {
                   backgroundImage: `url(${data?.product?.headerImg?.url})`,
                 }}
                 className="product-detail-hero"
-              >
-                <div className="product-detail-hero-overlay" />
-              </div>
+              ></div>
 
               <div className="product-detail-navigator">
                 <div className="product-detail-navigator-item">
@@ -86,76 +81,70 @@ const ProductDetail: React.FC<ProductDetailProps> = () => {
                   </div>
                 </Block>
               </Content>
-              <Block first last />
+              <Block first />
               <Block />
 
-              <Content size="4XL" className="product-detail-description">
-                <Block first last />
-                <Block first />
-                <Block id={'overview'} className="product-detail-overview">
-                  <div className="product-detail-overview-description">
-                    <h2>Overview</h2>
-                    <hr />
-                    <Markdown>{data?.product?.description}</Markdown>
-                  </div>
-                </Block>
-                <Block first />
-                <Block
-                  id={'specification'}
-                  first
-                  last
-                  className="product-detail-overview"
-                >
-                  {data?.product?.specification?.map((spec, key) => (
-                    <div key={key}>
-                      <div className="product-detail-overview-img">
-                        <Yoga maxCol={2}>
-                          <div className="product-detail-overview-description">
-                            <h2>{spec?.title}</h2>
-                            <hr />
-                            <Markdown>{spec?.description}</Markdown>
-                          </div>
-                          <div>
-                            <Carousel
-                              enableAutoPlay={true}
-                              pagination={false}
-                              renderArrow={myArrow}
-                              itemsToShow={1}
-                            >
-                              {spec?.specImages?.map((img, key) => (
-                                <div
-                                  key={key}
-                                  className={'spec-imgs'}
-                                  style={{
-                                    backgroundImage: `url(${img?.url})`,
-                                  }}
-                                />
-                              ))}
-                            </Carousel>
-                          </div>
-                        </Yoga>
-                      </div>
+              <Block>
+                <Content size="4XL" className="product-detail-description">
+                  <Block id={'overview'} className="product-detail-overview">
+                    <div className="product-detail-overview-description">
+                      <h2>Overview</h2>
+                      <Markdown>{data?.product?.description}</Markdown>
                     </div>
-                  ))}
-                </Block>
-
-                <Block first last className="center">
-                  <Button
-                    to={`${data?.product?.brochure?.url}`}
-                    download
-                    mode="primary-outline"
+                  </Block>
+                  <Block first last />
+                  <Block
+                    id={'specification'}
+                    first
+                    last
+                    className="product-detail-overview"
                   >
-                    Download Brochure
-                  </Button>
-                </Block>
-                {/* <Scene>
-                  <Entity
-                  primitive='a-sky' 
-                  src="../../../../../static/360/showroom.jpg"
-                  // rotation="0 -130 0"
-                  />
-                </Scene> */}
-              </Content>
+                    {data?.product?.specification?.map((spec, key) => (
+                      <div key={key}>
+                        <div className="product-detail-overview-img">
+                          <Yoga maxCol={2}>
+                            <div className="product-detail-overview-description">
+                              <h2>{spec?.title}</h2>
+                              <Markdown>{spec?.description}</Markdown>
+                            </div>
+                            {!spec?.specImages ||
+                            spec?.specImages?.length === 0 ? null : (
+                              <div>
+                                <Carousel
+                                  enableAutoPlay={true}
+                                  pagination={false}
+                                  renderArrow={myArrow}
+                                  itemsToShow={1}
+                                >
+                                  {spec?.specImages?.map((img, key) => (
+                                    <div
+                                      key={key}
+                                      className={'spec-imgs'}
+                                      style={{
+                                        backgroundImage: `url(${img?.url})`,
+                                      }}
+                                    />
+                                  ))}
+                                </Carousel>
+                              </div>
+                            )}
+                          </Yoga>
+                        </div>
+                      </div>
+                    ))}
+                  </Block>
+
+                  <Block first last className="center">
+                    <Button
+                      to={`${data?.product?.brochure?.url}`}
+                      download
+                      mode="primary-outline"
+                    >
+                      Download Brochure
+                    </Button>
+                  </Block>
+                </Content>
+              </Block>
             </div>
           )}
         </div>
